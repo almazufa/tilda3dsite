05a9486195a746147a1dcfa975806d97a843d3ee
almazufa <120163826+almazufa@users.noreply.github.com>
2023-07-24 17:00:23 +0300

    Update MyProject3-HTML5-Shipping.UE4.js

--- MyProject3-HTML5-Shipping.UE4.js
+++ MyProject3-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('MyProject3-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/almazufa/tilda3dsite/35ef129c97619db7d93f6c58469a336ef0d8e3d2/MyProject3-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
