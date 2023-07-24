3ced78e3093a46116236f8b28d9e930de474fd6d
almazufa <120163826+almazufa@users.noreply.github.com>
2023-07-24 16:55:33 +0300

    Update MyProject3-HTML5-Shipping.data.js

--- MyProject3-HTML5-Shipping.data.js
+++ MyProject3-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'C:/Users/gaska/Documents/Unreal Projects/gaskarov/Binaries/HTML5/MyProject3-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'MyProject3-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/scl/fi/rfqwafqowpue4msmdv1nl/MyProject3-HTML5-Shipping.data?rlkey=3lu541dl2hgh6h5hbwnx1r5v9&dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/scl/fi/rfqwafqowpue4msmdv1nl/MyProject3-HTML5-Shipping.data?rlkey=3lu541dl2hgh6h5hbwnx1r5v9&dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
